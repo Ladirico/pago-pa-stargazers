cf005822e07373576d6496d62a125b6cabeb23ad
Ladirico <alice.dirico@outlook.it>
2023-03-07 19:45:13 +0100

    change from pagoPaStartgazers to pagoPaStargazers

--- android/app/src/main/java/com/pagopastartgazers/MainActivity.java
+++ android/app/src/main/java/com/pagopastartgazers/MainActivity.java
@@ -1,4 +1,4 @@
-package com.pagopastartgazers;
+package com.pagoPaStargazers;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -12,7 +12,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "pagoPaStartgazers";
+    return "pagoPaStargazers";
   }
 
   /**
--- ios/pagoPaStartgazers/AppDelegate.mm
+++ ios/pagoPaStartgazers/AppDelegate.mm
@@ -44,7 +44,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 #endif
 
   NSDictionary *initProps = [self prepareInitialProps];
-  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"pagoPaStartgazers", initProps);
+  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"pagoPaStargazers", initProps);
 
   if (@available(iOS 13.0, *)) {
     rootView.backgroundColor = [UIColor systemBackgroundColor];
